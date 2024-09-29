d60beb06379e5b5226725d0586739724ca23b38b
ishant9805 <ishant9805@gmail.com>
2024-09-29 06:09:26 +0530

    Add CRUD features added crud features to tweets used bootstrap modals for tweet deletion used django forms for tweet creation and updation

--- vispr/tweet/urls.py
+++ vispr/tweet/urls.py
@@ -4,5 +4,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.tweet_list, name='tweet_list'),
+    path('tweet_create/', views.tweet_create, name='tweet_create'),
+    path('tweet_edit/<int:tweet_id>', views.tweet_edit, name='tweet_edit'),
+    path('tweet_delete/<int:tweet_id>', views.tweet_delete, name='tweet_delete'),
 ]
